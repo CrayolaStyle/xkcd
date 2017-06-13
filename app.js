7ff32246222c27cc3c1ef1296707856cf618e892
James Schultz <ninjames101@gmail.com>
2017-06-12 19:13:21 -0600

    😀  just messing with formatting

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     console.log('ready')
     const variable = randomIntFromInterval(1,600);
     $.get('https://galvanize-cors.herokuapp.com/https://xkcd.com/'+variable+'/info.0.json').then(function(data) {
+      console.log(variable);
     console.log(data);
+    const header = $('<h2>');
+    header.text(data.title)
+    $('body').append(header)
     const imgHolder = $('<img>');
     imgHolder.attr("src", data.img)
     $('body').append(imgHolder)
